8d7721ed4421d3cce872471a49a8eff58f142dd7
mariabrock <mariabrock414@gmail.com>
2019-10-07 21:03:44 -0500

    added event listeners to paint boxes

--- src/javascripts/components/paintbrush/paintbrush.js
+++ src/javascripts/components/paintbrush/paintbrush.js
@@ -1,13 +1,14 @@
+import $ from 'jquery';
 import utilities from '../../helpers/utilities';
 
-const colors = ['darkorchid', 'chartruse', 'papayawhip', 'tomato', 'cornsilk', 'almond'];
+const colors = ['darkorchid', 'chartreuse', 'papayawhip', 'tomato', 'cornsilk', 'sienna'];
 
 const printColorPicker = () => {
   let domString = '';
   for (let i = 0; i < colors.length; i += 1) {
     const color = colors[i];
     domString += `<div class="form-check form-check-inline">
-  <input name="color" class="form-check-input clolor-choice" type="radio" id="${color}" value="${color}">
+  <input name="color" class="form-check-input color-choice" type="radio" id="${color}" value="${color}">
   <label class="form-check-label" for="${color}">${color}</label>
 </div>
 `;
@@ -15,4 +16,14 @@ const printColorPicker = () => {
   utilities.printToDom('color-picker', domString);
 };
 
-export default { printColorPicker };
+const paintPixel = (event) => {
+  const pixel = $(event.target);
+  const colorChoice = $('.color-choice:checked').val();
+  pixel.css('backgroundColor', colorChoice);
+};
+
+const attachEvents = () => {
+  $(document).on('click', 'td', paintPixel);
+};
+
+export default { printColorPicker, attachEvents };
